4bcc8a7343dccb42d6ec7b1605e113d27fa6b316
zhouyu <zhouyu@berkeley.edu>
2023-10-30 15:03:11 -0700

    add geoserver for testing

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -11,19 +11,19 @@ ruby bin/import solr tmp/test_public/
 echo "index solr ends"
 
 # publish geoserver (need testing both public and restricted geoserver?)
-# echo "3.1 - create geoserver workspace ..."
-# ruby bin/import geoserver_workspace UCB
-# echo "create create geoserver workspace ends"
+echo "3.1 - create geoserver workspace ..."
+ruby bin/import geoserver_workspace UCB
+echo "create create geoserver workspace ends"
 
-# echo "3.2 - publish vector to geoserver starting..."
-# ruby bin/import geoserver aaaa.shp
-# echo "publish vector to geoserver ends"
+echo "3.2 - publish vector to geoserver starting..."
+ruby bin/import geoserver fk4cr7f93g.shp
+echo "publish vector to geoserver ends"
 
 # echo "3.3 - publish raster to geoserver starting..."
 # ruby bin/import geoserver bbbb.TIFF
 # echo "publish raster to geoserver ends"
 
-
+# import all with a zipfile
 echo "4 - import all starting..."
 ruby bin/import all test_public.zip
 echo "import all ends"
