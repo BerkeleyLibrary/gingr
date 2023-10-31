46f2f9a1b443f69f53dd4d3394af6206f1a2329a
zhouyu <zhouyu@berkeley.edu>
2023-10-31 16:05:02 -0700

    solr_indexer spec

--- lib/solr_indexer.rb
+++ lib/solr_indexer.rb
@@ -22,6 +22,7 @@ module Gingr
       doc = JSON.parse(File.read(file_path))
       [doc].flatten.each do |record|
         update_reference_urls!(record) if @need_update_reference_urls
+        puts record
         @solr.update params: { commitWithin: commit_within, overwrite: true },
                      data: [record].to_json,
                      headers: { 'Content-Type' => 'application/json' }
@@ -32,11 +33,30 @@ module Gingr
 
     def update_reference_urls!(record)
       references = record['dct_references_s']
+
       Config.reference_urls.each do |name, from_url|
         to_url = @reference_urls[name.to_s]
-        references.gsub(from_url, to_url) if to_url
+        references = references.gsub(from_url, to_url) if to_url
       end
       record['dct_references_s'] = references
     end
+
+    # def update_reference_urls!(record)
+    #   puts('inin')
+    #   references = record['dct_references_s']
+    #   puts references.class
+    #   Config.reference_urls.each do |name, from_url|
+    #     puts name
+    #     to_url = @reference_urls[name.to_s]
+    #     puts from_url
+    #     puts to_url
+    #     # references.gsub(from_url, to_url) if to_url
+    #     references.gsub(from_url, to_url)
+    #   end
+    #   puts references
+    #   record['dct_references_s'] = references
+
+    #   # puts record
+    # end
   end
 end
--- spec/solr_indexer_spec.rb
+++ spec/solr_indexer_spec.rb
@@ -1,9 +1,41 @@
 require 'spec_helper'
+require_relative '../lib/solr_indexer'
 
 module Gingr
   describe SolrIndexer do
-    let(:reference_urls) do
-      { {"geoserver_secure"=>"http://geoserver", "geoserver"=>"http://geoserver:8080", "download"=>"https://spatial.lib.berkeley.edu"} }
+    let(:url) { ENV.fetch('SOLR_URL') || 'http://solr:8983/solr/geodata-test' }
+    let(:file_path) { 'spec/fixture/jsonfile/berkeley_public_pdf.json' }
+    context '# not empty reference_urls' do
+      let(:reference_urls) do
+        { 'geoserver_secure' => 'http://fake_geoserver_secure:8081',
+          'geoserver' => 'http://fake_geoserver:8080',
+          'download' => 'https://fake_spatial.lib.berkeley.edu' }
+      end
+      let(:solr_indexer) { SolrIndexer.new(url, reference_urls) }
+      let(:record) do
+        doc = JSON.parse(File.read(file_path))
+        [doc].flatten[0]
+      end
+
+      it 'should call the update reference field method' do
+        expect(solr_indexer).to receive(:update_reference_urls!).with(record)
+        solr_indexer.update(file_path)
+      end
+
+      it 'shoule update reference field' do
+        solr_indexer.send(:update_reference_urls!, record)
+        # change to not use this long string when getting real data
+        updated_reference_field = '{"http://www.opengis.net/def/serviceType/ogc/wfs":"http://fake_geoserver:8080/wfs","http://www.opengis.net/def/serviceType/ogc/wms":"http://fake_geoserver:8080/wms","http://www.isotc211.org/schemas/2005/gmd/":"https://spatial.ucblib.org/metadata/berkeley-s7038h/iso19139.xml","http://schema.org/downloadUrl":"https://spatial.ucblib.org/public/berkeley-s7038h/data.zip","http://lccn.loc.gov/sh85035852":"https://spatial.ucblib.org/public/berkeley-s7038h/x.pdf"}'
+        expect(record['dct_references_s']).to eq updated_reference_field
+      end
+    end
+
+    context '# empty reference_urls' do
+      it 'should not call the update reference field method' do
+        solr_indexer = SolrIndexer.new(url, {})
+        solr_indexer.update(file_path)
+        expect(solr_indexer).not_to receive(:update_reference_urls!)
+      end
     end
   end
 end
