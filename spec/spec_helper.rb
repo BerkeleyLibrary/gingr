43df7fc21cd75e12061e5d3515470e67d4c04838
Daniel Schmidt <dcschmidt@berkeley.edu>
2023-12-18 21:48:33 -0800

    Load Docker secrets in tests and the gingr command

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+require 'berkeley_library/docker'
+BerkeleyLibrary::Docker::Secret.load_secrets!
+
 $LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
 require 'gingr'
 
